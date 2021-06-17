f42086c78d5db94c41bc7ac497303be8d426bceb
Dryss <andrys.magar@hotmail.fr>
2021-06-17 13:03:23 +0200

    Fin de UseState /ajout des projets/ projet useState

--- react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("je suis dryyss");
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: `je suis dryyss`,
+  });
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "hello dryyss" });
+    setMessage("hello dryyss");
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
