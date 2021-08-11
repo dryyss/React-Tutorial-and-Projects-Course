ad4bc2f4fbb775aa6c6a4e95fc40e44cb3e4a3c9
Dryss <andrys.magar@hotmail.fr>
2021-08-11 02:54:37 +0200

    NavBar

--- react-projects-master/09-color-generator/setup/src/SingleColor.js
+++ react-projects-master/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1500);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${index > 10 && "color-lignt"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
