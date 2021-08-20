2696e3d2564cbb4bb99ba160de2cf0ca7e751974
Dryss <andrys.magar@hotmail.fr>
2021-08-20 17:05:09 +0200

    stripe project

--- react-projects-master/13-stripe-submenus/setup/src/Hero.js
+++ react-projects-master/13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import { useGlobalContext } from "./context";
+import phoneImg from "./images/phone.svg";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Infrastructure de paiement pour le commerce en ligne</h1>
+          <p>
+            Des millions d'entreprises de toutes tailles, des start-up aux
+            grandes entreprises, utilisent le logiciel et les API de Stripe pour
+            accepter des paiements, effectuer des virements et gérer leurs
+            activités en ligne.
+            <button className="btn">Démarrer maintenant</button>
+          </p>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- react-projects-master/13-stripe-submenus/setup/src/context.js
+++ react-projects-master/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,45 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinate) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinate);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSidebar,
+        openSubmenu,
+        closeSubmenu,
+        closeSidebar,
+        location,
+        page,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
